cd2d1679fe51f7ca97c7cc29211261a47e97a93d
Jobson Lira <jobson.lira@gmail.com>
2022-04-09 19:05:33 -0300

    chore: working with audio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
   constructor() {
 
+    this._audio = new Audio('click.mp3'); 
+    this._audioOnOff = false;
     this._lastOperator = '';
     this._lastNumber = '';
 
@@ -47,10 +49,35 @@ class CalcController {
 
     this.setLastNumberToDisplay();
     this.pasteFromClipBoard();
+
+    document.querySelectorAll('.btn-ac').forEach(btn =>{
+        btn.addEventListener('dblclick', e =>{
+
+          this.toggleAudio(); 
+
+        });
+    });
+  }
+
+  toggleAudio(){
+
+     this._audioOnOff = !this._audioOnOff;
+
+  }
+
+  playAudio(){
+    if (this._audioOnOff){
+      
+       this._audio.currentTime = 0;
+       this._audio.play();
+
+    }
   }
 
   initKeyBoard() {
     document.addEventListener('keyup', e => {
+      
+      this.playAudio();
 
       switch (e.key) {
         case 'Escape':
@@ -267,6 +294,9 @@ class CalcController {
   }
 
   execBtn(value) {
+
+    this.playAudio(); 
+
     switch (value) {
       case 'ac':
         this.clearAll();
